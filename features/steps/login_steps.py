cad47de18eb9ceae2299f5a7d9351c4dbb9f1b6a
FesalProgrammer <FesalProgramming@gmail.com>
2026-06-21 16:30:59 -0400

    behave

---
+++ features/steps/login_steps.py
@@ -0,0 +1,33 @@
+from pages.login_page import LoginPage
+from behave import given,when,then 
+
+@given("que el usuario esta en la pagina de Login")
+def step_usuario_en_login(context):
+    context.login_page = LoginPage(context.driver)
+    context.login_page.open_page()
+
+@when("ingresa el usuario '{usuario}' y la contraseña '{password}'")
+def step_ingresar_credenciales(context,usuario,password):
+    if usuario == "VACIO":
+        usuario = ""
+
+    if password == "VACIO":
+        password = ""
+
+    context.login_page.ingresar_usuario(usuario)
+    context.login_page.ingresar_password(password)
+
+
+@when("hace clic en el boton Login")
+def step_clic_login(context):
+    context.login_page.click_in_login()
+
+@then("deberia ingresar al inventario")
+def step_validar_login_exitoso(context):
+    assert "/inventory.html" in context.driver.current_url, "No se redirigió al inventario"
+
+@then("deberia ver el mensaje de error '{mensaje}'")
+def step_validar_mensaje_error(context,mensaje):
+    error = context.login_page.get_error_message()
+    assert mensaje in error, f"Se esperaba '{mensaje}' y se obtuvo '{error}'"
+
