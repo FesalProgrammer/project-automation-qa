c70da5e1cf52b50060cf38bc55a4effd0199164e
FesalProgrammer <FesalProgramming@gmail.com>
2026-06-27 14:25:52 -0400

    github actions

--- features/steps/api_steps.py
+++ features/steps/api_steps.py
@@ -5,7 +5,7 @@ headers = {
     
   "x-api-key":"pub_0c467ebc212bdc0fc96774a8af22442e253722fef14060fe1fd499f4b3c96284"
 }
-#Sceneario 1
+# Sceneario 1
 @given("la API de Reqres esta disponible")
 def step_connecting_api_reqres(context):
     context.base_url =("https://reqres.in/api")
@@ -22,13 +22,13 @@ def step_login(context):
                          json=body
     )
 
-@then("el status code debe ser {status_code:d}")# :d convierte string a int y el :f convierte a floate.
+@then("el status code debe ser {status_code:d}")
 def step_validation_status(context, status_code):
-    print(f"status: {context.response.status_code}")#sirve para ver que codigo retorna y es el string 200.
+    print(f"status: {context.response.status_code}")
     assert context.response.status_code == status_code
 
-#Sceneario 2
-# No se hace el given porque es el mismo anterior.
+# Sceneario 2
+
 @when("realizar un login sin contraseña")
 def step_login_no_password(context):
     body={
@@ -40,10 +40,10 @@ def step_login_no_password(context):
                          json=body
     )
 
-# No se hace uno de los then porque es el mismo anterior.
+
 
 @then("el mensaje de error debe ser '{mensaje}'")
 def step_validation_msj(context, mensaje):
     body = context.response.json()
-    print(f"status: {context.response.status_code}") #sirve para ver que codigo retorna y es el string 400.
     assert body["error"] == mensaje
+    
